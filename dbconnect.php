47f787282d0c000f4fdd89b7fc60201aaa74b607
qudrat0712538 <q.u.arain@rgu.ac.uk>
2017-04-20 12:51:33 +0100

    Dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?
 $connectstr_dbhost = 'MYSQLCONNSTR_localdb';
 $connectstr_dbname = 'studentportal';
-$connectstr_dbusername = '';
+$connectstr_dbusername = 'root';
 $connectstr_dbpassword = '';
 foreach ($_SERVER as $key => $value) {
     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
